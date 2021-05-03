01972a383eec3f7733e16873950127f2fe692cc0
Adrian Liu <adrianliu99@berkeley.edu>
2021-05-02 22:06:34 -0700

    PPO2 e2e agent

--- ROAR/agent_module/rl_depth_e2e_agent.py
+++ ROAR/agent_module/rl_depth_e2e_agent.py
@@ -6,6 +6,8 @@ from ROAR.utilities_module.vehicle_models import Vehicle, VehicleControl
 
 class RLDepthE2EAgent(Agent):
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
+        super(RLDepthE2EAgent, self).run_step(vehicle=vehicle,
+                                         sensors_data=sensors_data)
         if self.kwargs.get("control") is None:
             return VehicleControl()
         else:
